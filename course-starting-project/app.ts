1b59fd0e10b40739c04155f3d9e7c6c6dcb6b840
Michael Erwin <michaelerwinconsulting@gmail.com>
2023-09-07 19:23:26 +0200

    27 functions as types + 28 func types & callbacks

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -6,6 +6,22 @@ function printResult(num: number): void {
   console.log("Result: " + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
+
 printResult(add(5, 12));
 
 // let someValue: undefined;
+
+//let combineValues: Function;
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+// combineValues = printResult;
+// combineValues = 5;
