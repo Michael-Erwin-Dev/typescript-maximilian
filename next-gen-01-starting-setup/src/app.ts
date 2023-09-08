8f6b5064df165756095f29780774769fff83ccaa
Michael Erwin <michaelerwinconsulting@gmail.com>
2023-09-08 18:10:03 +0200

    52 spread operator

--- next-gen-01-starting-setup/src/app.ts
+++ next-gen-01-starting-setup/src/app.ts
@@ -41,3 +41,15 @@ if (button) {
 }
 
 printOutput(add(5));
+
+const hobbies = ["Sports", "Cooking"];
+const activeHobbies = ["Hiking"];
+
+activeHobbies.push(...hobbies);
+
+const person = {
+  firstName: "Max",
+  age: 30,
+};
+
+const copiedPerson = { ...person };
